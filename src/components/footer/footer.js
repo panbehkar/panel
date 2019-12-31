a682e8b659b3dedcc6f8936f881f8aac3f93dcca
Peyman <abolfazl.panbehkar@yahoo.com>
2019-12-31 16:25:42 +0330

    fix footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-	const year = new Date().getFullYear();
-	return (
-		<footer className="p-footer">
-			<span>
-				Copyright &copy; {year}{" "}
-				<a href="http://pnr.ir" target="_blank" rel="noopener noreferrer">
-					PNR
-				</a>{" "}
-				Institute.
-			</span>
-		</footer>
-	);
+    const year = new Date().getFullYear();
+    return (
+        <footer className="p-footer">
+            <span>
+                Copyright &copy; {year}{" "}
+                <a
+                    href="http://panbehkar.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Panbehkar
+                </a>
+            </span>
+        </footer>
+    );
 };
 
 export default Footer;
